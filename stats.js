37beca6dd31a67e2aeeca1fa536b2dc5eb30ce05
GigioR <gioprib@gmail.com>
2020-05-22 09:26:15 -0300

    'commit'

---
+++ stats.js
@@ -0,0 +1,29 @@
+const os = require('os');
+
+//console.log(os.platform());
+
+setInterval( () => {
+    const { freemem, totalmem} = os;
+
+//console.log(freemem(), totalmem());
+
+    const total = parseInt(totalmem() / 1024 / 1024);
+    const mem = parseInt(freemem() / 1024 / 1024);
+    const percents = parseInt((mem / total) * 100);
+
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents} %`
+    }
+    
+    console.clear();
+    console.log("===== PC Stats =====");
+    console.table(stats);
+
+}, 1000)
+
+
+
+//console.log(mem, total, percents)
+
